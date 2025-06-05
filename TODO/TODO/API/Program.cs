9f0a41b2dec01d725d6a4c339748577222e74a37
CAIO MURILO KURIKI <caio.murilo@cs.up.edu.br>
2025-06-05 09:30:34 -0300

    criação do post

--- TODO/TODO/API/Program.cs
+++ TODO/TODO/API/Program.cs
@@ -8,10 +8,20 @@ builder.Services.AddDbContext<AppDataContext>();
 
 var app = builder.Build();
 
-app.MapGet("/API/Tarefa",
+app.MapGet("/api/tarefas",
 ([FromServices]AppDataContext ctx) =>
-Result.Ok(ctx.Tarefa.ToList())
+Results.Ok(ctx.Tarefa.ToList())
 );
 
+app.MapPost("/api/tarefas",
+    ([FromBody]Tarefas tarefas,[FromServices] AppDataContext ctx) =>
+        var tarefa = ctx.Tarefas.Find(Tarefa.StatusId);
+        if(Status == null){
+            return Result.NotFound();
+            tarefa.Status = Status;
+            ctx.Tarefa.Add(tarefa);
+            ctx.SaveChanges();
+            return Results.Created($"/api/tarefas/tarefas.Id", tarefa);       
+    });
 
 app.Run();

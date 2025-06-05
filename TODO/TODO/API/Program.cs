abfcb280040e216408d717e7ce4a3771e30c728a
CAIO MURILO KURIKI <caio.murilo@cs.up.edu.br>
2025-06-05 09:53:54 -0300

    criacao do put por id

--- TODO/TODO/API/Program.cs
+++ TODO/TODO/API/Program.cs
@@ -9,8 +9,8 @@ builder.Services.AddDbContext<AppDataContext>();
 var app = builder.Build();
 
 app.MapGet("/api/tarefas",
-([FromServices]AppDataContext ctx) =>
-Results.Ok(ctx.Tarefa.ToList())
+    ([FromServices]AppDataContext ctx) =>
+        Results.Ok(ctx.Tarefa.ToList())
 );
 
 app.MapPost("/api/tarefas",
@@ -32,6 +32,29 @@ app.MapGet("/api/tarefas/{id}", ([FromRoute] string id,
     {
         return Results.NotFound();
     }
+    return Results.Ok(tarefa);
+});
+
+app.MapPut("/api/tarefas/{id}", ([FromRoute] string id,
+    [FromBody] Tarefa tarefaAlterado,
+    [FromServices] AppDataContext ctx) =>
+{
+    Tarefa?  = ctx.Tarefas.Find(id);
+    if (tarefa == null)
+    {
+        return Results.NotFound();
+    }
+    Status? status = ctx.Status.Find(tarefa.StatusId);
+    if (status is null)
+    {
+        return Results.NotFound();
+    }
+    tarefa.Status = status;
+    tarefa.Titulo = tarefaAlterado.Titulo;
+    tarefa.Status = tarefaAlterado.Status;
+    tarefa.DataVencimento = tarefaAlterado.DataVencimento;
+    ctx.Tarefas.Update(tarefa);
+    ctx.SaveChanges();
     return Results.Ok(tarefa);
 });
 
